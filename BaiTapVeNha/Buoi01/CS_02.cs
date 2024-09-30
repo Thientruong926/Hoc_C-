d2eb263c063643216c48c4d8ab28bc839040f52c
Thientruong926 <huathientruong123@gmail.com>
2024-09-30 16:48:09 +0700

    Update CS_02.cs

--- BaiTapVeNha/Buoi01/CS_02.cs
+++ BaiTapVeNha/Buoi01/CS_02.cs
@@ -1,1 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace CS01_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap tham so a: ");
+            double a=double.Parse(Console.ReadLine());
+            Console.Write("Nhap tham so b: ");
+            double b=double.Parse(Console.ReadLine());
+            if (a == 0)
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("Phuong trinh co vo so nghiem");
+                }
+                else
+                {
+                    Console.WriteLine("Phuong trinh vo nghiem");
+                }
+            }
+            else
+            {
+                double x = -b / a;
+                Console.WriteLine("Phuong trinh co nghiem x= " + x);
+            }
+            Console.ReadKey();
+        }
+    }
+}
