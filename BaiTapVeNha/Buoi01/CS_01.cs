c939aa0b620e06eb9afb2fa022717569e07291ec
Thientruong926 <huathientruong123@gmail.com>
2024-09-30 16:47:17 +0700

    Update CS_01.cs

--- BaiTapVeNha/Buoi01/CS_01.cs
+++ BaiTapVeNha/Buoi01/CS_01.cs
@@ -1,1 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace CS01_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ten: ");
+            string name=Console.ReadLine();
+            Console.WriteLine("Xin chao "+name);
+            Console.ReadKey();
+        }
+    }
+}
